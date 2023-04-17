f6a6a0f81dddfb3704b824f83f4843445aa9977e
tkreker <tetiana.kreker@gmail.com>
2023-04-17 22:57:01 +0300

    hw1_edited

--- pages/register.js
+++ pages/register.js
@@ -7,23 +7,23 @@ emailField: {xpath: '//*[@id="input-email"]'},
 phoneNumberField: {xpath: '//*[@id="input-telephone"]'},
 passwordField: {xpath: '//*[@id="input-password"]'},
 passwordRepeatField: {xpath: '//*[@id="input-confirm"]'},
-subscribeRatioButton: {xpath: '//*[@id="content"]/form/fieldset[3]/div/div/label[2]'},
-iHaveReadRatioButton: {xpath: '//*[@id="content"]/form/div/div/input[1]'},
+subscribeRadioButton: {xpath: '//*[@id="content"]/form/fieldset[3]/div/div/label[2]'},
+iHaveReadRadioButton: {xpath: '//*[@id="content"]/form/div/div/input[1]'},
 continueButton: {xpath: '//*[@id="content"]/form/div/div/input[2]'},
 
     verifyRegisterPage() {
       I.see('Регистрация');
     },
 
-    fillNewUserForm(user) {
+    submitNewUserForm(user) {
       I.fillField(this.firstNameField, user.firstName);
     I.fillField(this.lastNameField, user.lastName);
     I.fillField(this.emailField, user.email);
     I.fillField(this.phoneNumberField, user.phone);
     I.fillField(this.passwordField, user.password);
-    I.fillField(this.passwordRepeatField, user.passwordRepeat);
-    I.click(this.subscribeRatioButton);
-    I.click(this.iHaveReadRatioButton);
+    I.fillField(this.passwordRepeatField, user.password);
+    I.click(this.subscribeRadioButton);
+    I.click(this.iHaveReadRadioButton);
     I.click(this.continueButton);  
     },
 
