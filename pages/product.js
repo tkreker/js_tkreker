191cd12a1b778b4b91c13ac9d884d907790cdd2e
tkreker <tetiana.kreker@gmail.com>
2023-04-25 18:45:59 +0300

    .

--- pages/product.js
+++ pages/product.js
@@ -12,20 +12,20 @@ module.exports = {
   },
 
   async getProductPrice() {
-    const productPriceString = await this.grabTextFrom({ xpath: '//*[@id="content"]/div[1]/div[2]/div/div[1]/span[1]' });
-    const colorPriceString = await this.grabTextFrom({ xpath: "//a[contains(text(),'Black') and contains(text(), '(+$') and contains(@class, 'sbSelector')]" });
+    const productPriceString = await I.grabTextFrom({ xpath: '//*[@id="content"]/div[1]/div[2]/div/div[1]/span[1]' });
+    const colorPriceString = await I.grabTextFrom({ xpath: "//a[contains(text(),'Black') and contains(text(), '(+$') and contains(@class, 'sbSelector')]" });
     const productPrice = I.parsePrice(productPriceString);
     const colorPrice = I.parsePrice(colorPriceString);
     return totalPrice = productPrice + colorPrice;
   },
 
   async getCheckoutPrice() {
-    const checkoutPriceString = await this.grabTextFrom({ xpath: '//*[@id="collapse-checkout-confirm"]/div/div[1]/table/tfoot/tr[3]/td[2]' });
+    const checkoutPriceString = await I.grabTextFrom({ xpath: '//*[@id="collapse-checkout-confirm"]/div/div[1]/table/tfoot/tr[3]/td[2]' });
     return checkoutPrice = I.parsePrice(checkoutPriceString);
   },
 
   async getShippingPrice() {
-    const shippingPriceString = await this.grabTextFrom({ xpath: '//*[@id="collapse-checkout-confirm"]/div/div[1]/table/tfoot/tr[2]/td[2]' });
+    const shippingPriceString = await I.grabTextFrom({ xpath: '//*[@id="collapse-checkout-confirm"]/div/div[1]/table/tfoot/tr[2]/td[2]' });
     return shippingPrice = I.parsePrice(shippingPriceString);
   },
 
